ba4935d7ca0af55a4f3280922872a1893f62de40
Pave1iOS <PGribachev94@yandex.ru>
2025-01-05 17:13:07 +0300

    chapter 23 + load main apps in launcher

--- app/src/main/java/com/example/personallauncher/LauncherActivity.kt
+++ app/src/main/java/com/example/personallauncher/LauncherActivity.kt
@@ -1,6 +1,7 @@
 package com.example.personallauncher
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.PowerManager
 import android.util.Log
